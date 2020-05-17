5c55abed8a74a92a93af8c7270dee41119ad9ae4
Paul WOISARD <paulwoisard@gmail.com>
2020-05-17 12:25:44 +0200

    ImagesForDark

--- js/styleswitcher.js
+++ js/styleswitcher.js
@@ -1,12 +1,19 @@
 var buttonChangeStyle = document.getElementById("replaceonclicktest");
 buttonChangeStyle.onclick = function changementdestyle() {
 
+    //var ghLink = document.getElementsById("Github2Dark");
+    //var lpLink = document.getElementsById("Launchpad2Dark");
+
     var baliseLink = document.getElementById("linkcss");
     var etat = baliseLink.href.endsWith("css/style.css");
 
     if (etat == true) {
         baliseLink.setAttribute("href", "css/style-dark.css");
+        document.getElementById("Github2Dark").src = "images/OpenSource/Github-dark.png";
+        document.getElementById("Launchpad2Dark").src = "images/OpenSource/Launchpad-dark.png";
     } else {
         baliseLink.setAttribute("href", "css/style.css");
+        document.getElementById("Github2Dark").src = "images/OpenSource/Github.png";
+        document.getElementById("Launchpad2Dark").src = "images/OpenSource/Launchpad.png";
     }
 }

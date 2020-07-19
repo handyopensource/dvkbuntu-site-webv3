36c4eee41ebfd0e23eef466a936480a8eb654b77
Paullux <paulwoisard@gmail.com>
2020-07-19 21:41:03 +0200

    exclusion .ftpquota nodejs app

--- nodejs/index.js
+++ nodejs/index.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const dirTree = require("directory-tree");
 
 const directoryPath = path.join(__dirname, '../downloads');
-const filteredTree = dirTree(directoryPath, { exclude: /index.html|listefichiers.json|index.js/ });
+const filteredTree = dirTree(directoryPath, { exclude: /.ftpquota|index.html|listefichiers.json|index.js/ });
 
 fs.writeFile('listefichiers.json', JSON.stringify(filteredTree), function(err) {
     if(err) return console.log(err)

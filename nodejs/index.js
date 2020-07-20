757bd889dda5bcf5cab97923517fb30d5807a158
Paullux <paulwoisard@gmail.com>
2020-07-20 09:53:17 +0200

    ajout express.js

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,11 +1,17 @@
+const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const dirTree = require("directory-tree");
+const app = express();
 
-const directoryPath = path.join(__dirname, '../downloads');
-const filteredTree = dirTree(directoryPath, { exclude: /.ftpquota|index.html|listefichiers.json|index.js/ });
+app.get('/', function (req, res) {
+  const directoryPath = path.join(__dirname, '../downloads');
+  const filteredTree = dirTree(directoryPath, { exclude: /.ftpquota|index.html|listefichiers.json|index.js/ });
 
-fs.writeFile('listefichiers.json', JSON.stringify(filteredTree), function(err) {
-    if(err) return console.log(err)
-    console.log('ok')
-});
+  fs.writeFile('listefichiers.json', JSON.stringify(filteredTree), function(err) {
+    if(err) return res.send(err)
+    res.send('ok')
+  });
+})
+
+app.listen(3000)

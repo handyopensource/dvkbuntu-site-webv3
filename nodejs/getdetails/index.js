feb759b01b7906fd6bce190973602917e01694fb
Paullux <paulwoisard@gmail.com>
2020-07-24 19:52:07 +0200

    séparation en 3 du nodejs et utilisation de get

---
+++ nodejs/getdetails/index.js
@@ -0,0 +1,56 @@
+if (typeof(PhusionPassenger) !== 'undefined') {
+    PhusionPassenger.configure({ autoInstall: false });
+}
+
+const express = require('express');
+const app = express();
+
+const mysql = require('mysql');
+
+function makeConnection(){
+  return mysql.createConnection({
+    host     : process.env.DB_HOST,
+    user     : process.env.DB_USER,
+    password : process.env.DB_PASS,
+    database : process.env.DB_NAME
+  });
+}
+
+app.get('/nodejs/gettotal', async function(req, res) {
+  getTotal(req, res);
+});
+
+async function getTotal(req, res){
+  var connection = makeConnection();
+
+  var startDate = req.query.startdate
+  var endDate = req.query.enddate
+  var send = {}
+
+  if(Date.parse(startDate) && Date.parse(endDate) && (Date.parse(startDate) <= Date.parse(endDate))){
+    connection.query(`select COUNT(*) as total from downloads where dateTime BETWEEN '${startDate}' AND  '${endDate}'`, function (error, results, fields) {
+      if (error)
+        send = {status: 'error'}
+      else{
+        send = {status: 'ok', result: results}
+      }
+      res.send(send)
+    });
+  }else{
+    connection.query('select COUNT(*) as total from downloads', function (error, results, fields) {
+      if (error)
+        send = {status: 'error'}
+      else{
+        send = {status: 'ok', result: results}
+      }
+      res.send(send)
+    });
+  }
+  connection.end();
+}
+
+if (typeof(PhusionPassenger) !== 'undefined') {
+    app.listen('passenger');
+} else {
+    app.listen(3000);
+}

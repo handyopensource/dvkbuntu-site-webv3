dc44b9df8f0b6159f0e2434b0b45ab9b1fd26549
Joey JEANTET <jeantetjoey.pro@gmail.com>
2020-07-24 11:46:34 +0200

    ajout de l'api downloads

--- nodejs/index.js
+++ nodejs/index.js
@@ -9,6 +9,8 @@ const path = require('path');
 const fs = require('fs');
 const dirTree = require("directory-tree");
 
+const mysql = require('mysql');
+
 
 app.get('/nodejs', function (req, res) {
   const directoryPath = path.join(__dirname, '../downloads');
@@ -19,6 +21,114 @@ app.get('/nodejs', function (req, res) {
     res.send('ok');
   });
 });
+
+function makeConnection(){
+  return mysql.createConnection({
+    host     : process.env.DB_HOST,
+    user     : process.env.DB_USER,
+    password : process.env.DB_PASS,
+    database : process.env.DB_NAME
+  });
+}
+
+app.post('/downloads/:method', async function(req, res) {
+  var method = req.params.method
+  switch(method.toLowerCase()){
+    case 'addone':{
+      addOne(req, res);
+      break;
+    }
+    case 'gettotal':{
+      getTotal(req, res);
+      break;
+    }
+    case 'getdetails':{
+      getDetails(req, res);
+      break;
+    }
+    default:{
+      res.send('Wrong method')
+      break;
+    }
+  }
+});
+
+
+async function addOne(req, res){
+  var connection = makeConnection();
+  var send = {}
+  connection.query('insert into downloads (dateTime) values (NOW());', function (error, results, fields) {
+    if (error){
+      send = {status: 'error'};
+      console.log(error);
+    }else{
+      send = {status: 'ok'}
+    }
+    res.send(send)
+  });
+  connection.end();
+}
+
+async function getTotal(req, res){
+  var connection = makeConnection();
+
+  var startDate = req.query.startdate
+  var endDate = req.query.enddate
+  var send = {}
+
+  if(Date.parse(startDate) && Date.parse(endDate) && (Date.parse(startDate) <= Date.parse(endDate))){
+    connection.query(`select COUNT(*) as total from downloads where dateTime BETWEEN '${startDate}' AND  '${endDate}'`, function (error, results, fields) {
+      if (error) 
+        send = {status: 'error'}
+      else{
+        send = {status: 'ok', result: results}
+      }
+      res.send(send)
+    });
+  }else{
+    connection.query('select COUNT(*) as total from downloads', function (error, results, fields) {
+      if (error) 
+        send = {status: 'error'}
+      else{
+        send = {status: 'ok', result: results}
+      }
+      res.send(send)
+    });
+  }
+  connection.end();
+}
+
+async function getDetails(req, res){
+  var connection = makeConnection();
+
+  var startDate = req.query.startdate
+  var endDate = req.query.enddate
+
+  if(Date.parse(startDate) && Date.parse(endDate) && (Date.parse(startDate) <= Date.parse(endDate))){
+    connection.query(`select * from downloads where dateTime BETWEEN '${startDate}' AND  '${endDate}'`, function (error, results, fields) {
+      if (error) 
+        send = {status: 'error'}
+      else{
+        send = {status: 'ok', result: results}
+      }
+      res.send(send)
+    });
+  }else{
+    connection.query('select * from downloads', function (error, results, fields) {
+      if (error) 
+        send = {status: 'error'}
+      else{
+        send = {status: 'ok', result: results}
+      }
+      res.send(send)
+    });
+  }
+
+  connection.end();
+}
+
+
+
 
 if (typeof(PhusionPassenger) !== 'undefined') {
     app.listen('passenger');

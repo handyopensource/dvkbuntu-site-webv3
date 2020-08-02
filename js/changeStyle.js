4e4a55cf80a5846c1a68681ce32de57174c4191b
Paullux <paulwoisard@gmail.com>
2020-08-02 22:22:15 +0200

    amélioration loader

--- js/changeStyle.js
+++ js/changeStyle.js
@@ -2,7 +2,9 @@ function changementdestyle() {
 
   //var ghLink = document.getElementsById("Github2Dark");
   //var lpLink = document.getElementsById("Launchpad2Dark");
+  $('body').css('overflow', 'hidden');
 
+  $('.loader_bg').fadeIn(0);
   var baliseLink = document.getElementById("linkcss");
   var etat = baliseLink.href.endsWith("css/style.css");
   var imageExist = document.getElementById("Github2Dark");
@@ -19,4 +21,7 @@ function changementdestyle() {
       document.getElementById("Launchpad2Dark").src = "images/OpenSource/Launchpad.png";
     }
   }
+  $('.loader_bg').fadeOut(5000);
+
+  $('body').css('overflow', 'auto');
 }

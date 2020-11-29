26ca30bb2d00baffdd81534b0a5727dce20cdccd
Paullux <paulwoisard@gmail.com>
2020-11-29 16:34:46 +0100

    Réparation de la fonction de liste des fichiers à télécharger

--- nodejs/listFile/index.js
+++ nodejs/listFile/index.js
@@ -16,7 +16,7 @@ app.get('/nodejs/listFile', function (req, res) {
   const directoryPath = path.join(__dirname, '../../downloads');
   const filteredTree = dirTree(directoryPath, { exclude: /.ftpquota|.htaccess|index.html|listefichiers.json|index.js|style-dark.css|style.css|font-awesome.min.css|fontawesome-webfont.eot|fontawesome-webfont.svg|fontawesome-webfont.ttf|fontawesome-webfont.woff|fontawesome-webfont.woff2|FontAwesome.otf|IcoMoon-Free.ttf|css|fonts/ });
 
-  fs.writeFile('../../listefichiers.json', JSON.stringify(filteredTree), function(err) {
+  fs.writeFile(path.resolve(__dirname, '..', '..','listefichiers.json'), JSON.stringify(filteredTree), function(err) {
     if(err) return res.send(err);
     res.send('ok');
   });
